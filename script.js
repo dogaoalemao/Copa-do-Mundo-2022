d787c47a9dc7d30b239b671ad59f53e0aa870b08
Douglas Fernandes <dogaoalemao@gmail.com>
2023-02-07 10:51:07 -0300

    remover um elemento

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 let paragrafo = document.querySelector('vaiSerCampeao');
 
 console.log(paragrafo);
+
+
+// 3 
+let apagaVaiSerCampeao = document.querySelector('#vaiSerCampeao')
+
+apagaVaiSerCampeao.remove();
+
+
+// atividade
+// 1 - selecionassem um elemento
+// 2 - alterar o conteúdo de um elemento
+// 3 - removerem um elemento
+// 4 - desafio! criar um botão que mostre algo na tela
